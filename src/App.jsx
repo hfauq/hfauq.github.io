6ebed6be1e7249b6e5166e8737c1474e15eb125e
Hugo Fauquenoi <hugo@doist.com>
2026-04-16 16:22:21 +0200

    Add 2026 podcast appearance

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,11 @@ function App() {
         <div className="mb-[54px]">
           <Section label="Podcast">
             <div className="space-y-[24px]">
+              <PodcastItem
+                title="Just Now Possible with Teresa Torres (English)"
+                url="https://www.producttalk.org/building-todoist-ramble-how-doist-turned-voice-braindumps-into-real-time-task-capture/"
+                date="2026"
+              />
               <PodcastItem 
                 title="Growth Makers (French)"
                 url="https://growthmakers.fr/ep-46-hugo-fauquenoi-doist-30000-signups/"
